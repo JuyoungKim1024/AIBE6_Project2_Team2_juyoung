27dd96463077a8fd38dcc89e677ed9ac0fec1b2a
Kim Juyoung <op45672@gmail.com>
2026-04-16 16:20:30 +0900

    시스템생성

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,6 +4,7 @@ import com.domain.article.Article;
 import com.domain.article.ArticleController;
 import com.domain.article.ArticleRepository;
 import com.domain.article.ArticleService;
+import com.domain.system.SystemController;
 import com.util.Rq;
 import java.time.LocalDate;
 import java.util.Scanner;
@@ -13,6 +14,7 @@ public class App {
     private final ArticleRepository articleRepository = new ArticleRepository();
     private final ArticleService articleService = new ArticleService(articleRepository);
     private final ArticleController articleController = new ArticleController(scanner, articleService);
+    private final SystemController systemController = new SystemController();
 
     public void run() {
         System.out.println("=== 게시판 프로그램 ===");
@@ -29,10 +31,7 @@ public class App {
                 case "detail" -> articleController.detail(rq.getParam());
                 case "update" -> articleController.update(rq.getParam());
                 case "delete" -> articleController.delete(rq.getParam());
-                case "exit" -> {
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-                }
+                case "exit" -> systemController.actionExit();
                 default -> System.out.println("존재하지 않는 명령어입니다.");
             }
         }
